4105b00678c213106cfc92ee20597172140b08fb
Mert Erogu <merteroglu16@gmail.com>
2017-03-02 02:36:13 +0300

    En kisa yol Fonksiyonu eklendi

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #define INF 999999
+#define N 23
 
 int sehirKoduBul(char sehirKoduDizi[][50],char *sehirAdi,int maxSehir){
 
@@ -15,9 +16,70 @@ int sehirKoduBul(char sehirKoduDizi[][50],char *sehirAdi,int maxSehir){
     return 0;
 }
 
+int dijsktra(int maliyetDizisi[][N],int merkezSehir,int hedefSehir,char sehirDizisi[50][50])
+{
+    int mesafe[N],onceki[N],gidilmis[N]={0},baslangic;
+    char yol[N];
+    for(int i=1;i< N;i++)
+    {
+        mesafe[i] = INF;
+        onceki[i] = -1;
+    }
+    baslangic = merkezSehir;
+    gidilmis[baslangic]=1;
+    mesafe[baslangic] = 0;
+    int min , m , d;
+    while(gidilmis[hedefSehir] ==0)
+    {
+        min = INF;
+        m = 0;
+        for(int i=1;i< N;i++)
+        {
+            d = mesafe[baslangic] + maliyetDizisi[baslangic][i];
+            if(d < mesafe[i] && gidilmis[i]==0)
+            {
+                mesafe[i] = d;
+                onceki[i] = baslangic;
+            }
+            if(min>mesafe[i] && gidilmis[i]==0)
+            {
+                min = mesafe[i];
+                m = i;
+            }
+        }
+        baslangic = m;
+        gidilmis[baslangic] = 1;
+    }
+    baslangic = hedefSehir;
+   int j = 0;
+    while(baslangic != -1)
+    {
+        yol[j++] = baslangic+65;
+        baslangic = onceki[baslangic];
+    }
+
+    yol[j]='\0';
+    strrev(yol);
+    //printf("%s", path);
+    yoluYazdir(yol,sehirDizisi);
+    return mesafe[hedefSehir];
+}
+
+void yoluYazdir(char path[50],char dizi[50][50]){
+
+    for(int i = 0;i<50;i++){
+        if(path[i] == '\0'){
+            break;
+        }
+        printf("%s-",dizi[path[i]-65]);
+    }
+
+}
 
 int main()
 {
+     char buff[255];
+
     FILE *sehirKodlari,*sehirMesafeler;
 
     sehirKodlari = fopen("\sehir kod.txt","r");
@@ -26,7 +88,7 @@ int main()
         printf("Sehir kodlari dosyasi bulunamadi!\n");
     }
 
-    char buff[255];
+
     char * kod;
     char * sehiradi;
 
@@ -38,7 +100,7 @@ int main()
       kod = strtok(NULL,"-");
       int i = atoi(kod);
       strcpy(dizi[i],sehiradi);
-      //printf("%s , %s\n",sehiradi,kod);
+      printf("%s - %s\n",kod,sehiradi);
     }
 
     int toplamSehir = atoi(kod) + 1;
@@ -74,15 +136,11 @@ int main()
 
     }
 
-    for(int i = 0;i<toplamSehir;i++){
-        for(int j = 0;j<toplamSehir;j++){
-          if(maliyetDizi[i][j] != INF){
-           // printf("%d - %d : %d\n",i,j,maliyetDizi[i][j]);
-          }
+   // printf("%d - %d - %d\n",1,3,maliyetDizi[1][3]);
 
-        }
-    }
 
+    int sonuc = dijsktra(maliyetDizi,1,17,dizi);
+    printf("\n%d",sonuc);
 
     return 0;
 }
